2c3680043c9b378e966e1706d790c8c7ab4da87f
Mistrbarta <AdamBarta12@seznam.cz>
2023-03-29 21:01:01 +0200

    CSS

--- src/components/Support-page/Sup.js
+++ src/components/Support-page/Sup.js
@@ -8,11 +8,11 @@ function Sup() {
             <div className="title-sup">
               <p>Contact Us</p>
             </div>
-            <form className="contact-form" action="index.html" method="post">
+            <form className="contact-form" method="get">
               <input type="text" className="contact-form-text" placeholder="Enter your name"/>
               <input type="email" className="contact-form-text" placeholder="Enter your email"/>
               <textarea className="contact-form-text" placeholder="Your problem" defaultValue={""} />
-              <input type="submit" className="contact-form-btn" defaultValue="Send" />
+              <input type="Submit" className="contact-form-btn" defaultValue="Send" />
             </form>
           </div>
       </div>
--- src/components/Support.js
+++ src/components/Support.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Sup from './Support-page/Sup';
+import Footer from './Footer/Footer';
 
 function Support() {
     return (
       <div>
         <Sup />
+        <Footer />
       </div>
     );
   }
